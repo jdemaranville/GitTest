2c241795a0f2211cef42d9fd6d53087e0489f2dd
Jeff DeMaranville <jdemaranville@centriq.com>
2020-11-06 07:45:45 -0600

    Initial commit

---
+++ GitTest.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitTest.UI.MVC.MvcApplication" Language="C#" %>
---
+++ GitTest.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTest.UI.MVC.Startup))]
+namespace GitTest.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
